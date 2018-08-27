8a8993a72fe55414cb6a1bde084ed945e580320a
droidkfx <kam157190@gmail.com>
2018-08-27 10:43:01 -0600

    Fix casing on windows

---
+++ src/templates/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return <footer className="footer">This page was created by Kyle Myers Ben saw this. Last updated on : ~~~BUILD_DATE_REPLACE_SLOT~~~ </footer>;
+  }
+}
+
+export default Footer;
