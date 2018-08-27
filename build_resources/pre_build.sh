7a07f98a99cbc48a89611ae615d92b056e1ffd65
droidkfx <kam157190@gmail.com>
2018-08-27 10:46:37 -0600

    update file name

--- build_resources/pre_build.sh
+++ build_resources/pre_build.sh
@@ -5,4 +5,4 @@ echo "Running pre build script...";
 # Update the footer build date to the current date
 DATE=`date`;
 echo "Replacing build date in footer as : $DATE";
-sed -ri "s/~~~BUILD_DATE_REPLACE_SLOT~~~/$DATE/" src/templates/footer.js;
+sed -ri "s/~~~BUILD_DATE_REPLACE_SLOT~~~/$DATE/" src/templates/Footer.js;
